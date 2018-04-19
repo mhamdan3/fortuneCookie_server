76f0da031e5d7cf335f5496f595d81a5ffa6a045
Mahdi Hamdan <hamdani0430@gmail.com>
2018-04-19 19:25:35 -0400

    init

---
+++ fortuneCookie_server.js
@@ -0,0 +1,23 @@
+
+var http = require("http");
+var PORT1 = 7000;
+var PORT2 = 7500;
+
+function handleRequest(request, response){
+    response.end("You're great!");
+};
+
+function handleRequest2(request, response){
+    response.end("Thanks Bro!");
+}
+
+var server = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
+
+server.listen(PORT1, function(){
+    console.log("You're great!");
+});
+
+server2.listen(PORT2, function(){
+    console.log("You're the best!");
+})
